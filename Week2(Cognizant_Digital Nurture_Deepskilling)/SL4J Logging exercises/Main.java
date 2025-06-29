aedeecc395dab05c50614bef79566cfa13510ac5
SivagunanithiR <154575573+SivagunanithiR@users.noreply.github.com>
2025-06-29 10:47:24 +0530

    Delete Week2(Cognizant_Digital Nurture_Deepskilling) directory

--- Week2(Cognizant_Digital Nurture_Deepskilling)/SL4J Logging exercises/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        LoginService service = new LoginService();
-
-        service.login("admin", "admin123");     // Should log INFO
-        service.login("admin", "wrongpass");     // Should log ERROR
-        service.login(null, null);               // Should log WARN
-    }
-}
